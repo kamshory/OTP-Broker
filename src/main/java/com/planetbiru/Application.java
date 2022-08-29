2e9c7b29bbcf1e22c67fdba4aaabadc2587b4956
kamshory <kamshory@gmail.com>
2022-08-29 15:26:56 +0700

    Update

--- src/main/java/com/planetbiru/Application.java
+++ src/main/java/com/planetbiru/Application.java
@@ -12,6 +12,10 @@ import com.planetbiru.config.ConfigNetDHCP;
 import com.planetbiru.config.ConfigNetEthernet;
 import com.planetbiru.config.ConfigNetWLAN;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigurationProperties
@@ -131,6 +135,11 @@ public class Application {
 			ConfigNetWLAN.apply(Config.getOsWLANConfigPath(), Config.getOsWLANConfigPath());
 			ConfigNetEthernet.apply(Config.getOsEthernetConfigPath());
 		}		
+		
+		Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer htmlRenderer = HtmlRenderer.builder().build();
+        return htmlRenderer.render(document);
 	}
 	
 	private static boolean usbPluged() 
